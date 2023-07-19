0ac9aaf61fed7e7f32a00087054158c4da44f78e
Aliya Sardar <aliyasardar8@gmail.com>
2023-07-19 20:54:38 +0500

    numpy array made writeable

--- garden_care.py
+++ garden_care.py
@@ -1,31 +1,29 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.models import load_model
 from tensorflow.keras.applications.resnet50 import preprocess_input
 import numpy as np
 import os
 from PIL import Image
 
-def recognize(file):
+def recognize(image_data):
     class_names = ['Grape___Black_rot', 'Grape___healthy', 'Peach___Bacterial_spot', 'Peach___healthy',
                    'Rose___Black_spot', 'Rose___healthy', 'Tomato___Late_blight', 'Tomato___healthy']
 
     # Load the TFLite model and allocate tensors.
     directory_path = os.path.dirname(__file__)
-    file_path = os.path.join(directory_path, 'TFlite_Model.tflite')
+    file_path = os.path.join(directory_path, 'Resnet50.tflite')
     interpreter = tf.lite.Interpreter(model_path=file_path)
     interpreter.allocate_tensors()
 
-    # Define the target image size
+    # Open the image from image_data using PIL
+    pil_image = Image.open(image_data)
+
+    # Resize the image to the target size
     target_size = (224, 224)
+    pil_image = pil_image.resize(target_size)
 
-    # Convert the FileStorage object to a NumPy array
-    image = Image.open(file)
-    image_array = np.array(image)
-
-    # Resize and preprocess the image
-    image_array = tf.image.resize(image_array, target_size)
-    image_array = img_to_array(image_array)
+    # Convert the PIL Image to a NumPy array
+    image_array = img_to_array(pil_image)
     image_array = np.expand_dims(image_array, axis=0)
     image_array = preprocess_input(image_array)
 
@@ -39,8 +37,8 @@ def recognize(file):
     # Run the inference
     interpreter.invoke()
 
-    # Get the output tensor and predicted class index
-    output_tensor = interpreter.get_tensor(output_details[0]['index'])
+    # Get the output tensor and make a writable copy
+    output_tensor = interpreter.get_tensor(output_details[0]['index']).copy()
     predicted_class_index = np.argmax(output_tensor[0])
 
     # Print the predicted class index
